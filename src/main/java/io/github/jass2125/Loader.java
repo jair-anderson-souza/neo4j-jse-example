ac4df6831f348b02d6581e048fd7ef34a63847d0
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-20 07:45:34 -0300

    fix

--- src/main/java/io/github/jass2125/Loader.java
+++ src/main/java/io/github/jass2125/Loader.java
@@ -7,7 +7,7 @@ public class Loader {
         Product product = new Product("1111", "product", 10);
         Product product2 = new Product("2222", "product2", 20);
         ProductService service = new ProductService();
-//        service.createProduct(product);
+        service.createProduct(product);
         service.createProduct(product2);
         
         Product prod = service.getProductById("1111");
